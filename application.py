a2dd36f8146e4b71970549c01896bb7eee61b928
lhcorreaz08 <l.correaz@uniandes.edu.co>
2023-04-15 10:46:15 -0500

    actualización app.py

--- application.py
+++ application.py
@@ -17,21 +17,21 @@ def set_env():
 
 set_env()
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
-app.config['PROPAGATE_EXCEPTIONS'] = True
+application = Flask(__name__)
+application.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
+application.config['PROPAGATE_EXCEPTIONS'] = True
 
-app_context = app.app_context()
+app_context = application.app_context()
 app_context.push()
 
-db.init_app(app)
+db.init_app(application)
 db.create_all()
 
-cors = CORS(app)
+cors = CORS(application)
 
-api = Api(app)
+api = Api(application)
 api.add_resource(VistaBlacklist, '/blacklist')
 api.add_resource(VistaBlacklistInformacion, '/blacklist/<email>')
 
@@ -39,6 +39,6 @@ api.add_resource(VistaBlacklistInformacion, '/blacklist/<email>')
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)        
+    application.run(debug=True, host='0.0.0.0', port=port)        
 
 
